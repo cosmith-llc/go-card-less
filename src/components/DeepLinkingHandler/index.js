91fea78a098025e1ed6f1258aa12133eeee022fc
Eduard Panasenko <asus_5000@icloud.com>
2025-04-28 12:04:11 +0300

    added global state for case when multiply component in one project

--- src/components/DeepLinkingHandler/index.js
+++ src/components/DeepLinkingHandler/index.js
@@ -24,19 +24,18 @@ const prepareParams = (url, props, params) => {
 function completeRedirectFlow(props, url) {
   if (!(url !== null && typeof url !== 'undefined')) return;
 
-  // fullDeepLink = `${uriSchema}://${uriHostname}/${uriPath}`
-  // this.state.currentUrl.toLowerCase().includes(this.state.redirectUrl)
   const { onDeepLinkingAction } = props;
   if (onDeepLinkingAction) {
-    //var url = "http://example.com?myVar=test&otherVariable=someData&number=123"
-    var regex = /[?&]([^=#]+)=([^&#]*)/g,
-      params = {},
-      match;
-    while (match = regex.exec(url)) {
-      params[match[1]] = match[2];
-    }
-    const values = prepareParams(url, props, params);
-    onDeepLinkingAction.apply(this, values);
+    setTimeout(() => {
+      let regex = /[?&]([^=#]+)=([^&#]*)/g,
+        params = {},
+        match;
+      while (match = regex.exec(url)) {
+        params[match[1]] = decodeURIComponent(match[2]);
+      }
+      const values = prepareParams(url, props, params);
+      onDeepLinkingAction.apply(this, values);
+    }, 200)
   }
 }
 
@@ -44,41 +43,70 @@ const useInitialURL = (props) => {
   console.log('useInitialURL:step1');
   const [url, setUrl] = useState(null);
   const [processing, setProcessing] = useState(true);
-
   console.log('useInitialURL:step2');
-  useEffect(() => {
+
+  if (!Linking.__handledUrls) {
     console.log('useInitialURL:step3');
+    Linking.__handledUrls = new Set();
+  }
+
+  useEffect(() => {
     const getUrlAsync = async () => {
-      console.log('useInitialURL:step4', Linking.__someProperty);
-      // Get the deep link used to open the app
-      const initialUrl = await Linking.getInitialURL();
+      logger.info(Linking.__latestUrlChecked);
+      if (!Linking.__latestUrlChecked) {
+        const initialUrl = await Linking.getInitialURL();
+        console.log(initialUrl);
+        Linking.__latestUrl = initialUrl;
+        Linking.__latestUrlChecked = true;
+      }
+
+      const initialUrl = Linking.__latestUrl;
+      console.log('useInitialURL:step4', Linking.__latestUrl);
       Linking.__someProperty = true;
       console.log('useInitialURL:step5', Linking.__someProperty);
-      // The setTimeout is just for testing purpose
+
       setTimeout(() => {
         console.log('useInitialURL:step6', initialUrl);
         setUrl(initialUrl);
-        console.log('useInitialURL:step7');
         setProcessing(false);
         console.log('useInitialURL:step8', initialUrl);
-        completeRedirectFlow(props, initialUrl);
+
+        if (initialUrl) {
+          !Linking.__handledUrls.has(initialUrl) && Linking.__handledUrls.add(initialUrl);
+          completeRedirectFlow(props, initialUrl);
+        }
+
         console.log('useInitialURL:step9');
       }, 100);
     };
-    if (Linking && !Linking.__someProperty) {
+
+    if (Linking.__latestUrl) {
+      setUrl(Linking.__latestUrl);
+      setProcessing(false);
+
+      if (!Linking.__handledUrls.has(Linking.__latestUrl)) {
+        Linking.__handledUrls.add(Linking.__latestUrl);
+        completeRedirectFlow(props, Linking.__latestUrl);
+      }
+    } else if (Linking && !Linking.__someProperty) {
       getUrlAsync();
     }
   }, []);
 
   useEffect(() => {
     const handleDeepLink = ({ url }) => {
+      Linking.__latestUrl = url;
       Linking.__someProperty = true;
       console.log('useInitialURL:step10', url);
       setUrl(url);
       console.log('useInitialURL:step11');
       setProcessing(false);
-      console.log('useInitialURL:step12', url);
-      completeRedirectFlow(props, url);
+
+      if (url && !Linking.__handledUrls.has(url)) {
+        console.log('useInitialURL:step12', url);
+        Linking.__handledUrls.add(url);
+        completeRedirectFlow(props, url);
+      }
     };
 
     const linkingSubscription = Linking.addEventListener('url', handleDeepLink);
